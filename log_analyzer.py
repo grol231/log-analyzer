117a73f2b8bd049f3d3d07c6444168c8cfe2faca
Grigoriy Kosterev <kosterev@yandex.ru>
2019-04-06 20:52:31 +0300

    log-analyzer v1.0

--- log_analyzer.py
+++ log_analyzer.py
@@ -15,37 +15,48 @@ import argparse
 import json
 import logging
 from collections import namedtuple
+from datetime import datetime
 
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "/home/kgm/otus/homework/badlog",
-    "LOG_FILE": ""
-    #"LOG_FILE": "./analyzer_log/log.txt"
+    "LOG_DIR": "./log",
+    "ANALYZER_LOG": ""
 }
-
 LOG_PARSING_ERROR_PER = 50
+REPORT_TEMPLATE_PATH = 'template/report.html'
+line_format = re.compile(r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) -  - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST|DELETE|PATCH|PUT) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["]-["]) (["](?P<refferer>(\-)|(.+))["]) (["]-["]) (["](?P<useragent>(\-)|(.+))["]) (["](?P<id>(\-)|(.+))["]) ((?P<time>(\-)|(.+)))""", re.IGNORECASE)
+log_nginx_format = re.compile(r"""nginx-access-ui.log-(?P<date>\d[0-9]+)(.gz|$)""")
 
-lineformat = re.compile(r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) -  - \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(GET|POST|DELETE|PATCH|PUT) )(?P<url>.+)(http\/1\.1")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["]-["]) (["](?P<refferer>(\-)|(.+))["]) (["]-["]) (["](?P<useragent>(\-)|(.+))["]) (["](?P<requestid>(\-)|(.+))["]) ((?P<requesttime>(\-)|(.+)))""", re.IGNORECASE)
 
-def fined_last_log():
-    Log = namedtuple('Log', ['path', 'date', 'type'])
-    log_file = Log('', '', '')
+def get_last_log(log_dir):
+    LogFile = namedtuple('LogFile', ['path', 'date', 'extension'])
+    last_log = None
+    for file_name in os.listdir(log_dir):
+        match = re.search(log_nginx_format, file_name)
+        if match is None:
+            continue
+        else:
+            date_str = match.groupdict()['date']
+            date = datetime.strptime(date_str, '%Y%m%d')
+        path = log_dir + '/' + file_name
+        extension = 'gz' if 'gz' == file_name[-2:] else 'plain'
+        if last_log:
+            if last_log.date < date:
+                last_log = LogFile(path, date, extension)
+        else:
+            last_log = LogFile(path, date, extension)
+    return last_log
 
-    return log_file
 
-def exists_file(filename, config_data):
-    date = filename[-8:]
-    day = date[-2:]
-    month = date[-4:-2]
-    year = date[0:4]
-    report_name = 'report_{}_{}_{}.html'.format(day, month, year)
-    return os.path.exists(config_data["REPORT_DIR"] + '/' + report_name)
+def is_processed(date, report_dir):
+    report_name = 'report_{}.html'.format(date.strftime('%Y%m%d'))
+    return os.path.exists(report_dir + '/' + report_name)
 
 
 def get_config():
-    parser = argparse.ArgumentParser(description='Process config.')
-    parser.add_argument('--config', dest='config', type=str, default='config/config.txt', help='an integer for the accumulator')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', dest='config', type=str, default='config/config.txt')
     args = parser.parse_args()
     config_file_path = args.config
     global config
@@ -66,164 +77,155 @@ def get_config():
         result['REPORT_SIZE'] = config_json['REPORT_SIZE']
     else:
         result['REPORT_SIZE'] = config['REPORT_SIZE']
-    if 'LOG_FILE' in config_json:
-        result['LOG_FILE'] = config_json['LOG_FILE']
-    elif config['LOG_FILE']:
-        result['LOG_FILE'] = config['LOG_FILE']
+    if 'ANALYZER_LOG' in config_json:
+        result['ANALYZER_LOG'] = config_json['ANALYZER_LOG']
+    elif config['ANALYZER_LOG']:
+        result['ANALYZER_LOG'] = config['ANALYZER_LOG']
     else:
-        result['LOG_FILE'] = None
+        result['ANALYZER_LOG'] = None
     return result
 
-def parse_log(file, log_dir):
-    if file.endswith(".gz"):
-        logfile = gzip.open(os.path.join(log_dir, file))
-    else:
-        logfile = open(os.path.join(log_dir, file))
+
+def parse_log(path, extension):
+    logfile = gzip.open(path) if 'gz' == extension else open(path)
     for l in logfile.readlines():
-        yield re.search(lineformat, l)
+        if 'gz' == extension:
+            yield re.search(line_format, l.decode('utf-8'))
+        else:
+            yield re.search(line_format, l)
+
+
+def get_url_counts(log):
+    url_counts = {}
+    for line in log:
+        if line['url'] in url_counts:
+            ++url_counts[line['url']]
+        else:
+            url_counts[line['url']] = 1
+    return url_counts
+
+
+def get_url_percents(url_counts, log_size):
+    url_percent = {}
+    for url in url_counts:
+        url_percent[url] = url_counts[url]/log_size * 100
+    return url_percent
+
+
+def get_url_time_sums(log):
+    url_time_sums = {}
+    for line in log:
+        if line['url'] in url_time_sums:
+            url_time_sums[line['url']] += float(line['time'])
+        else:
+            url_time_sums[line['url']] = float(line['time'])
+    return url_time_sums
+
+
+def get_request_common_time(url_time_sums):
+    request_common_time = 0
+    for url in url_time_sums:
+        request_common_time += url_time_sums[url]
+    return request_common_time
+
+
+def get_url_time_percents(url_time_sums, request_common_time):
+    url_time_percents = {}
+    for url in url_time_sums:
+        url_time_percents[url] = url_time_sums[url]/request_common_time * 100
+    return url_time_percents
+
+
+def get_url_avg_times(url_time_sums, url_counts):
+    url_avg_times = {}
+    for url in url_counts:
+        url_avg_times[url] = url_time_sums[url] / url_counts[url]
+    return url_avg_times
+
+
+def get_url_time_max(log):
+    url_time_max = {}
+    for line in log:
+        if line['url'] not in url_time_max:
+            url_time_max[line['url']] = line['time']
+        else:
+            if line['time'] > url_time_max[line['url']]:
+                url_time_max[line['url']] = line['time']
+    return url_time_max
+
+
+def get_url_time_medians(log):
+    request_times = {}
+    for line in log:
+        if line['url'] in request_times:
+            request_times[line['url']].append(float(line['time']))
+        else:
+            request_times[line['url']] = []
+            request_times[line['url']].append(float(line['time']))
+    url_time_medians = {}
+    for url in request_times:
+        url_time_medians[url] = statistics.median(request_times[url])
+    return url_time_medians
 
 
 def process(config_data):
-    print('Process log.')
-    for f in os.listdir(config_data["LOG_DIR"]):
-        if exists_file(f, config):
-            continue
-        arr = []
-        unparseable_line_count = 0
+    last_log = get_last_log(config_data['LOG_DIR'])
+    if last_log:
+        if is_processed(last_log.date, config_data['REPORT_DIR']):
+            logging.info('Log already processed.')
+            return
+        log = []
+        unhandled_line_count = 0
         line_count = 0
-        lines = parse_log(f, config_data['LOG_DIR'])
+        lines = parse_log(last_log.path, last_log.extension)
         for l in lines:
             line_count += 1
             if l:
-                arr.append(l.groupdict())
+                log.append(l.groupdict())
             else:
-                unparseable_line_count += 1
-    print('#####################################################################################################################################################')
-
-    if unparseable_line_count/line_count * 100 > LOG_PARSING_ERROR_PER:
-        logging.error("More than {} log parsing error percent.".format(LOG_PARSING_ERROR_PER))
+                unhandled_line_count += 1
+    if unhandled_line_count/line_count * 100 > LOG_PARSING_ERROR_PER:
+        logging.error("More than {}% lines with parsing errors.".format(LOG_PARSING_ERROR_PER))
         return
-
-    urlCounts = {}
-    for a in arr:
-        if a['url'] in urlCounts:
-            ++urlCounts[a['url']]
-        else:
-            urlCounts[a['url']] = 1
-
-   # for u in urlCounts:
-   #      print('url: {}, count: {}'.format(u, urlCounts[u]))
-
-    reportSize = len(arr)
-    # print('reportSize: {}'.format(reportSize))
-
-    urlPercent = {}
-    for c in urlCounts:
-        urlPercent[c] = urlCounts[c] / reportSize
-
-    print('#######################################################################################################################################################')
-
-    urlTimeSum = {}
-    for a in arr:
-        if a['url'] in urlTimeSum:
-            urlTimeSum[a['url']] += float(a['requesttime'])
-        else:
-            urlTimeSum[a['url']] = 0
-            urlTimeSum[a['url']] += float(a['requesttime'])
-
-    for t in urlTimeSum:
-        print('url: {}, sum: {}'.format(t, urlTimeSum[t]))
-
-    print('#######################################################################################################################################################')
-
-    for p in urlPercent:
-        print('url: {}, percent: {}'.format(p, urlPercent[p]))
-
-    requestCommonTime = 0
-    for t in urlTimeSum:
-        requestCommonTime += urlTimeSum[t]
-
-    urlTimePerc = {}
-    for t in urlTimeSum:
-        urlTimePerc[t] = urlTimeSum[t]/requestCommonTime
-
-    print('#########################################################################################################################################################')
-    for t in urlTimePerc:
-        print('url: {}, timePerc: {}'.format(t, urlTimePerc[t]))
-
-    urlAvgTime = {}
-    for c in urlCounts:
-        urlAvgTime[c] = urlTimeSum[c]/urlCounts[c]
-
-    urlTimeMax = {}
-    for a in arr:
-        time = a['requesttime']
-        if a['url'] not in urlTimeMax:
-            urlTimeMax[a['url']] = time
-        else:
-            if time > urlTimeMax[a['url']]:
-                urlTimeMax[a['url']] = time
-    print('##########################################################################################################################################################')
-    for m in urlTimeMax:
-        print('url: {}, timeMax: {}'.format(m, urlTimeMax[m]))
-
-    print('##########################################################################################################################################################')
-    for a in urlAvgTime:
-        print('url: {}, avgTime: {}'.format(a, urlAvgTime[a]))
-
-
-    urlReqTimesList = {}
-    for a in arr:
-        if a['url'] in urlReqTimesList:
-            urlReqTimesList[a['url']].append(float(a['requesttime']))
-        else:
-            urlReqTimesList[a['url']] = []
-            urlReqTimesList[a['url']].append(float(a['requesttime']))
-
-    urlTimeMed = {}
-    for url in urlReqTimesList:
-        urlTimeMed[url] = statistics.median(urlReqTimesList[url])
-
-    print('#'*100)
-    for m in urlTimeMed:
-        print('url: {}, med: {}'.format(m, urlTimeMed[m]))
-
-    str = open('/home/kgm/otus/homework/log-analyzer/template/report.html', 'r').read()
+    url_counts = get_url_counts(log)
+    url_percents = get_url_percents(url_counts, len(log))
+    url_time_sums = get_url_time_sums(log)
+    request_common_time = get_request_common_time(url_time_sums)
+    url_time_percents = get_url_time_percents(url_time_sums, request_common_time)
+    url_avg_times = get_url_avg_times(url_time_sums, url_counts)
+    url_time_max = get_url_time_max(log)
+    url_time_medians = get_url_time_medians(log)
+    report_url_time_sums = sorted(url_time_sums.items(), key=lambda kv: kv[1], reverse=True)[0:1000]
     table_json = []
-
-    sorted_url_time_sum = sorted(urlTimeSum.items(), key=lambda kv: kv[1], reverse=True)
-    print(sorted_url_time_sum)
-
-    finally_sorted_url_time_sum = sorted_url_time_sum[0:1000]
-
-    for url in finally_sorted_url_time_sum:
+    for s in report_url_time_sums:
+        url = s[0]
         line = {
-            'count': urlCounts[url[0]],
-             'time_avg': urlAvgTime[url[0]],
-            'time_max': urlTimeMax[url[0]],
-            'time_sum': urlTimeSum[url[0]],
-            'url': url[0],
-            'time_med': urlTimeMed[url[0]],
-            'time_perc': urlTimePerc[url[0]],
-            'count_perc': urlPercent[url[0]]
+            'count': url_counts[url],
+            'time_avg': url_avg_times[url],
+            'time_max': url_time_max[url],
+            'time_sum': s[1],
+            'url': url,
+            'time_med': url_time_medians[url],
+            'time_perc': url_time_percents[url],
+            'count_perc': url_percents[url]
         }
         table_json.append(line)
-
-    t = Template(str)
-    result = t.safe_substitute(table_json=table_json)
-    open(config['REPORT_DIR'] + '/' + 'report_24_03_2019.html', 'w').write(result)
+    report_template = open(REPORT_TEMPLATE_PATH, 'r').read()
+    template = Template(report_template)
+    report = template.safe_substitute(table_json=table_json)
+    open(config_data['REPORT_DIR'] + '/' + 'report_' + last_log.date.strftime('%Y%m%d') + '.html', 'w').write(report)
 
 
 def main():
     try:
         config_data = get_config()
         logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s', level=logging.INFO,
-                            datefmt='%Y.%m.%d %H:%M:%S', filename=config_data['LOG_FILE'])
+                            datefmt='%Y.%m.%d %H:%M:%S', filename=config_data['ANALYZER_LOG'])
         process(config_data)
     except Exception as e:
         logging.exception(e)
         return
-    logging.info('done')
+    logging.info('analyze completed successfully')
 
 
 if __name__ == "__main__":
